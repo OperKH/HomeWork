a602a3803e11d0f3ff671069729a389ba69a0876
Александр Житник <D:\Programs\The Bat!\Mail>
2015-12-05 22:57:47 +0200

    Rating refactoring

--- 8/1/main.js
+++ 8/1/main.js
@@ -2,83 +2,81 @@ var app = angular.module('app', []);
 
 app.controller('someController', ['$scope', '$rootScope', 'mainService', function($scope, $rootScope, mainService) {
     $scope.userRating = mainService.getUserRating();
-    // $rootScope.$on('dataLoaded', function() {
-    //     console.log($scope.userRating);
-    // });
 }]);
 
 app.service('mainService', ['$http', '$rootScope', function($http, $rootScope) {
-    var userRating = {
-        data: {}
-    };
+    var userRating = {};
+    var patches = [];
 
     this.getUserRating = function() {
         return userRating;
     };
-    $http.get('http://rating.smartjs.academy/rating?hardMode').then(function(response) {
-        userRating.data = response.data;
-        console.log("Start Version:", userRating.data.version);
-        $rootScope.$applyAsync();
-        // $rootScope.$emit('dataLoaded');
-    });
 
     var ws = new WebSocket('ws://rating.smartjs.academy/rating');
     ws.addEventListener("message", function(event) {
         console.log(event.data);
-
         var newData = JSON.parse(event.data);
-
         if (!newData.status) {
             updateData(newData);
         }
     });
 
-    var patches = [];
+    $http.get('http://rating.smartjs.academy/rating?hardMode').then(function(response) {
+        initUserRating(response.data);
+    });
+
+
     function updateData(newData) {
-        patches.push(newData);
-        var currentVersion = userRating.data.version;
-
-        if (currentVersion){
-
-            while (patches.length) {
-                var patch = patches.splice(0,1)[0];
-                var fromVersion = patch.fromVersion;
-                var toVersion = patch.toVersion;
-
-                if (userRating.data.version === fromVersion) {
-
-                    if (patch.updates.length) {
-
-
-                        patch.updates.forEach(function(updatedObj) {
-                            var id = updatedObj.id;
-
-
-                            userRating.data.records.forEach(function(oldObj, index) {
-                                if (oldObj.id === id) {
-                                    userRating.data.records[index].points = updatedObj.points;
-                                    // console.log(updatedObj.points);
-                                    return;
-                                }
-                            });
-
-
-                        });
-
-
-                    }
-                    userRating.data.version = toVersion;
-                    console.log('Patched to version:', toVersion);
-                    $rootScope.$applyAsync();
-
-                } else {
-                    console.warn('Patch Error!!!');
-                }
-
-            }
-
+        if (userRating.version){
+            patchUserRating(newData);
+        } else {
+            patches.push(newData);
         }
-
     }
+
+    function initUserRating(data) {
+        _.merge(userRating, data);
+        console.log("Start Version:", userRating.version);
+        if (patches.length) {
+            while (patches.length) {
+                var patch = patches.splice(0, 1)[0];
+                patchUserRating(patch);
+            }
+        } else {
+            userRatingSort();
+        }
+    }
+
+    function patchUserRating(patch) {
+        var currentVersion = userRating.version;
+        var fromVersion = patch.fromVersion;
+        var toVersion = patch.toVersion;
+
+        if (userRating.version === fromVersion && patch.updates.length) {
+
+            patch.updates.forEach(function(updatedObj) {
+                var id = updatedObj.id;
+                userRating.records.forEach(function(oldObj, index) {
+                    if (oldObj.id === id) {
+                        userRating.records[index].points = updatedObj.points;
+                        // console.log(updatedObj.points);
+                        return;
+                    }
+                });
+
+            });
+        } else if (userRating.version !== fromVersion) {
+            console.warn('Patch Error!!!');
+        }
+        userRating.version = toVersion;
+        console.log('Patched to version:', toVersion);
+        userRatingSort();
+        $rootScope.$applyAsync();
+    }
+
+    function userRatingSort(){
+        userRating.records = _.sortByOrder(userRating.records, 'points', 'desc');
+    }
+
 }]);
 
