90181fd53fd33ed7c1c09a75f5b98c3058b1b325
OperKH <oper.kh@gmail.com>
2016-01-26 16:51:03 +0200

    Initial app.

---
+++ AngularHW5/jedi-tool/src/app/main/main.controller.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('jediTool')
+    .controller('MainController', MainController);
+
+  /** @ngInject */
+  function MainController($timeout, webDevTec, toastr) {
+    var vm = this;
+  }
+})();
