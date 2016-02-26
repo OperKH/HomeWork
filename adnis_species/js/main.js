bb8b3b4e84ac9b0669747f817ec5ae0863423a82
OperKH <oper.kh@gmail.com>
2016-02-26 16:09:03 +0200

    initial

---
+++ adnis_species/js/main.js
@@ -0,0 +1,45 @@
+(function() {
+    var adnis = angular.module('adnis', ['ngResource', 'ui.bootstrap']);
+
+    adnis.controller('SpeciesCtrl', function($scope, SpeciesService, $uibModal) {
+        $scope.species = SpeciesService.query();
+        $scope.openModal = function(){
+            $uibModal.open({
+                templateUrl: 'templates/modalTemplate.html',
+                controller: function($scope, $uibModalInstance){
+                    $scope.formData = {
+                        color: generateRandomColor()
+                    };
+
+
+                    $scope.changeColor = function () {
+                        debugger;
+                    };
+                    $scope.randomColor = function() {
+                        $scope.formData.color = generateRandomColor();
+                    };
+
+                    $scope.ok = function () {
+
+                    };
+                    $scope.cancel = function() {
+                        $uibModalInstance.dismiss('cancel');
+                    };
+
+                    function generateRandomColor() {
+                        return "#" + ("000000" + (~~(Math.random()*0xffffff)).toString(16)).slice(-6);
+                    }
+
+                }
+            });
+        };
+    });
+
+    adnis.factory('SpeciesService', function($resource) {
+        return $resource('http://notification.systems/api/species/:id');
+    });
+
+    adnis.run(function($http){
+        $http.defaults.headers.common.Authorization = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.IjUzZjMzYzhkMjk0N2NkNDU2ZmI5NGJjMyI.jY0oeE2D3-D14d6Z2WqILKfWR5PTduau6R492czGJ80';
+    });
+})();
---
+++ adnis_symptoms/js/main.js
@@ -0,0 +1,85 @@
+(function() {
+    var adnis = angular.module('adnis', ['ngResource', 'ui.bootstrap']);
+
+    adnis.controller('SymptomsCtrl', function($scope, $uibModal, SymptomsService, elementsPerPageConst) {
+        var symptomsResource = {
+            $cancelRequest: function(){}
+        };
+        var queryParams = {
+            skip: 0,
+            limit: elementsPerPageConst
+        };
+
+        $scope.pagination = {
+            itemsPerPage: elementsPerPageConst,
+            currentPage: 1
+        };
+
+        $scope.getSymptoms = function() {
+            symptomsResource.$cancelRequest();
+
+            queryParams.skip = ($scope.pagination.currentPage - 1) * $scope.pagination.itemsPerPage;
+            // queryParams.limit = $scope.pagination.itemsPerPage;
+
+            symptomsResource = SymptomsService.query(queryParams);
+            symptomsResource.$promise.then(function() {
+                $scope.symptoms = symptomsResource;
+            });
+        };
+
+        $scope.getSymptoms();
+
+
+
+        $scope.remove = function() {
+            var selected = $scope.symptoms.filter(function(symptom){
+                return symptom.selected;
+            });
+
+            SymptomsService.remove({_id: "53f622a843e3552327c32ffbu"});
+        };
+
+        $scope.openModal = function(){
+            $uibModal.open({
+                templateUrl: 'templates/modalTemplate.html',
+                controller: function($scope, $uibModalInstance){
+                    $scope.ok = function () {
+
+                    };
+                    $scope.cancel = function() {
+                        $uibModalInstance.dismiss('cancel');
+                    };
+
+                }
+            });
+        };
+    });
+
+    adnis.factory('SymptomsService', function($resource) {
+        return $resource('http://adnis.smartjs.academy/list/:id', {}, {
+            'query': {
+                method: 'GET',
+                cancellable: true,
+                isArray: true,
+                transformResponse: function(data) {
+                    var dataArr = angular.fromJson(data);
+                    result = dataArr.data;
+                    result.$total = dataArr.total;
+                    return result;
+                },
+                interceptor: {
+                    response: function (response) {
+                        response.resource.$total = response.data.$total;
+                        return response.resource;
+                    }
+                }
+            },
+        });
+    });
+
+    adnis.constant('elementsPerPageConst', 10);
+
+    adnis.run(function($http){
+        // $http.defaults.headers.common.Authorization = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.IjUzZjMzYzhkMjk0N2NkNDU2ZmI5NGJjMyI.jY0oeE2D3-D14d6Z2WqILKfWR5PTduau6R492czGJ80';
+    });
+})();
